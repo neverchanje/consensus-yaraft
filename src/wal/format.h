a34c8f8cc3358324e183b444d6970762fe1d21d6
neverchanje <wutao163.com@163.com>
2017-04-25 18:13:40 +0800

    wal/format: use .h to document segment format.
    Define constants about header size of segment and entry in format.h.

---
+++ src/wal/format.h
@@ -0,0 +1,42 @@
+// Copyright 2017 Wu Tao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+namespace consensus {
+namespace wal {
+
+//  Format of the encoded log entry:
+//
+//  LogEntry  := LogHeader Data
+//  LogHeader := Crc32 Length
+//
+//  Crc32  -> 4 bytes, checksum of Data field
+//  Length -> 4 bytes, length of data
+//  Data   -> bytes,   byte-encoded yaraft.pb.Entry
+//
+//  Each segment composes of a series of log entries:
+//
+//  Segment := Header LogEntry*
+//  Header  := Committed
+//
+//  Committed -> bool, committed marker of a log segment, if it is true,
+//  all entries in the segment must have been committed.
+
+constexpr static size_t kSegmentHeaderSize = 1;
+
+constexpr static size_t kEntryHeaderSize = 8;
+
+}  // namespace wal
+}  // namespace consensus
