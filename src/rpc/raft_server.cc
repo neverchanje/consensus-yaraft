ecf50765b7318f87885a1c38efb8afdd531f9457
neverchanje <wutao163.com@163.com>
2017-06-19 18:22:53 +0800

    rpc: update rpc server.
    In this commit we focused on making the server runnable.
    - The server won't fail even if RawNode::Step returns error status, which is the result of a bad request. The server will keep running as long as possible.
    - Once the FSM goes error, the server will response with a error code indicating the reason.
    - Each of the request(yaraft::pb::Message) corresponds to a status response(rpc::pb::Response), which is not the actual response generated by the FSM. For each round of the RPC, a request from the client is regarded successful whenever it received an valid response. The client will retry when the request timeouts or the response is not "OK". On the server side, it will asynchronously send back the responses, and just as what we have done in sending a request, await for the status response.
    - All of the initialization works are assigned to RaftServiceImpl::New, in which it loads WALs, constructs RawNode, parses cluster configuration(--initial_cluster).
    - RaftServiceImpl::New fails fast, i.e., once an error occurs, it prints the error logs and abort the program.

--- src/rpc/raft_server.cc
+++ src/rpc/raft_server.cc
@@ -14,49 +14,142 @@
 
 #include "rpc/raft_server.h"
 #include "base/stl_container_utils.h"
-#include "rpc/peer.h"
+
+#include <base/env.h>
+#include <boost/algorithm/string/trim.hpp>
 
 namespace consensus {
 namespace rpc {
 
-// asynchronously broadcast all messages in `mails`.
+RaftServiceImpl::~RaftServiceImpl() {
+  STLDeleteContainerPointers(peerMap_.begin(), peerMap_.end());
+}
+
 Status RaftServiceImpl::broadcast(const std::vector<yaraft::pb::Message> &mails) {
   for (const auto &m : mails) {
-    CHECK(peerMap_.find(m.to()) != peerMap_.end());
+    CHECK(peerMap_.size() < m.to() && m.to() != 0);
     RETURN_NOT_OK(peerMap_[m.to()]->AsyncSend(m));
   }
   return Status::OK();
 }
 
-RaftServiceImpl::~RaftServiceImpl() {
-  STLDeleteContainerPairSecondPointers(peerMap_.begin(), peerMap_.end());
+bool RaftServiceImpl::broadcastAllReadyMails(yaraft::Ready *rd) {
+  auto st = broadcast(rd->messages);
+  if (!st.IsOK()) {
+    LOG(WARNING) << "RaftServiceImpl::broadcast: " << st;
+  }
+  rd->messages.clear();
+  return st.IsOK();
+}
+
+static void printFlags() {
+  FMT_LOG(INFO, "--wal_dir=\"{}\"", FLAGS_wal_dir);
+  FMT_LOG(INFO, "--name=\"{}\"", FLAGS_name);
+  FMT_LOG(INFO, "--heartbeat_interval={}ms", FLAGS_heartbeat_interval);
+  FMT_LOG(INFO, "--election_timeout={}ms", FLAGS_election_timeout);
+  FMT_LOG(INFO, "--initial_cluster=\"{}\"", FLAGS_initial_cluster);
+}
+
+RaftServiceImpl *RaftServiceImpl::New() {
+  printFlags();
+
+  FATAL_NOT_OK(Env::Default()->CreateDirIfMissing(FLAGS_wal_dir),
+               fmt::format("create wal_dir=\"{}\" if missing", FLAGS_wal_dir));
+  wal::WriteAheadLog *wal;
+  auto memstore = new ::yaraft::MemoryStorage;
+  FATAL_NOT_OK(wal::WriteAheadLog::Default(FLAGS_wal_dir, &wal, memstore),
+               fmt::format("initialize data from wal_dir=\"{}\"", FLAGS_wal_dir));
+
+  auto conf = new yaraft::Config;
+  conf->storage = memstore;
+  conf->electionTick = FLAGS_election_timeout;
+  conf->heartbeatTick = FLAGS_heartbeat_interval;
+
+  std::map<std::string, std::string> peerNameToIp;
+  FATAL_NOT_OK(ParseClusterMembershipFromGFlags(&peerNameToIp), "--initial_cluster parse failed.");
+  for (uint64_t i = 1; i <= peerNameToIp.size(); i++) {
+    conf->peers.push_back(i);
+  }
+
+  auto server = new RaftServiceImpl();
+  server->peerMap_.reserve(peerNameToIp.size());
+  uint64_t id = 0;
+  for (auto &e : peerNameToIp) {
+    ++id;
+
+    auto it = server->peerNameToId_.find(e.first);
+    CHECK(it == server->peerNameToId_.end());
+    server->peerNameToId_[e.first] = id;
+
+    // there's no need to create a connection to itself.
+    if (FLAGS_name != e.first) {
+      FMT_SLOG(INFO, "New peer {name: \"%s\", url: \"%s\"} added.", e.first, e.second);
+      server->peerMap_[id] = new Peer(server, e.second);
+    } else {
+      server->url_ = e.second;
+    }
+  }
+
+  conf->id = server->peerNameToId_[FLAGS_name];
+
+  auto node = new yaraft::RawNode(conf);
+  server->node_.reset(node);
+  server->wal_.reset(wal);
+  server->storage_ = memstore;
+
+  return server;
+}
+
+void RaftServiceImpl::Step(google::protobuf::RpcController *controller,
+                           const yaraft::pb::Message *request, pb::Response *response,
+                           google::protobuf::Closure *done) {
+  sofa::pbrpc::RpcController *cntl = static_cast<sofa::pbrpc::RpcController *>(controller);
+  FMT_LOG(INFO, "RaftService::Step(): message from {}: {}", cntl->RemoteAddress(),
+          yaraft::DumpPB(*request));
+
+  // handling request
+  handle(*request, response);
+  done->Run();
+}
+
+static pb::StatusCode yaraftErrorCodeToRpcStatusCode(yaraft::Error::ErrorCodes code) {
+  switch (code) {
+    case yaraft::Error::StepLocalMsg:
+      return pb::StepLocalMsg;
+    default:
+      LOG(FATAL) << "Unexpected error code: " << yaraft::Error::ToString(code);
+      return pb::OK;
+  }
+}
+
+void RaftServiceImpl::handle(const yaraft::pb::Message &request, pb::Response *response) {
+  auto s = node_->Step(const_cast<::yaraft::pb::Message &>(request));
+  if (UNLIKELY(!s.IsOK())) {
+    LOG(ERROR) << "RawNode::Step: " << s.ToString();
+    response->set_code(yaraftErrorCodeToRpcStatusCode(s.Code()));
+    return;
+  }
+
+  // immediately handle ready after performing an FSM transition
+  // TODO(optimize) batching this
+  yaraft::Ready *rd = node_->GetReady();
+  handleReady(rd);
+}
+
+void RaftServiceImpl::handleReady(yaraft::Ready *rd) {
+  using namespace yaraft::pb;
+
+  if (!rd->entries.empty()) {
+    FATAL_NOT_OK(wal_->AppendEntries(rd->entries), "WriteAheadLog::AppendEntries");
+  }
+
+  if (!rd->hardState) {
+  }
+
+  if (!rd->messages.empty()) {
+    broadcastAllReadyMails(rd);
+  }
 }
 
 }  // namespace rpc
 }  // namespace consensus
-
-int main() {
-  ::sofa::pbrpc::RpcServerOptions opts;
-  ::sofa::pbrpc::RpcServer rpcServer(opts);
-
-  // Start rpc server.
-  if (!rpcServer.Start("0.0.0.0:12321")) {
-    LOG(ERROR) << "start rpc failed";
-    return EXIT_FAILURE;
-  }
-
-  // Register service.
-  auto raftService = new consensus::rpc::RaftServiceImpl();
-  if (!rpcServer.RegisterService(raftService)) {
-    FMT_LOG(ERROR, "export service failed");
-    return EXIT_FAILURE;
-  }
-
-  // Wait signal.
-  rpcServer.Run();
-
-  // Stop rpc rpc.
-  rpcServer.Stop();
-
-  return 0;
-}
