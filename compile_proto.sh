0f541fc7f423befd0c4422dfcf84cb58fa1ac327
Wu Tao <wutao163.com@163.com>
2017-07-22 10:01:03 +0800

    pb: mv proto out from src/rpc/pb to src/pb

--- compile_proto.sh
+++ compile_proto.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd `dirname $0` | pwd)
 PROTOC=$SCRIPT_DIR/yaraft/build/third_parties/bin/protoc
 $PROTOC -I $SCRIPT_DIR/yaraft/build/third_parties/include \
-        -I $SCRIPT_DIR/src/rpc/pb \
+        -I $SCRIPT_DIR/src/pb \
         -I $SCRIPT_DIR/build/third_parties/include \
-        --cpp_out=$SCRIPT_DIR/src/rpc/pb \
-        $SCRIPT_DIR/src/rpc/pb/raft_server.proto
+        --cpp_out=$SCRIPT_DIR/src/pb \
+        $SCRIPT_DIR/src/pb/raft_server.proto
