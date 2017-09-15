bb2d73b0ba405cdc4bc5ebd2d25b13aff5f87755
Wu Tao <wutao163.com@163.com>
2017-09-15 12:16:10 +0800

    memkv: introduce a new application of consensus-yaraft, memkv

---
+++ apps/memkv/src/status.h
@@ -0,0 +1,33 @@
+// Copyright 2017 Wu Tao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <silly/status.h>
+
+namespace memkv {
+
+class Error {
+ public:
+  enum ErrorCodes {
+    OK,
+  };
+
+  static std::string toString(unsigned int code);
+};
+
+typedef silly::Status<Error> Status;
+
+template <typename T>
+using StatusWith = silly::StatusWith<Status, T>;
+
+}  // namespace memkv
